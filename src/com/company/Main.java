17c5183e0bf1e2cf8c27cdea74c7aff6659a30cb
Zahid097 <98472087+Zahid097@users.noreply.github.com>
2022-02-14 12:08:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Animal shark = new Shark(1500, "Green", true);
+        Animal eagle = new Eagle(16, "Black", 300);
+        Animal turtle = new Turtle(75, "Brown", 50);
+
+        Animal[] animals = {turtle, eagle, shark};
+
+        for (Animal s : animals) {
+            if (s instanceof Shark) {
+                ((Shark) s).attack();
+            }
+            if (s.getClass().getName().equals("com.company.Shark")) {
+                ((Shark) s).attack();
+            }
+
+            if (s instanceof Turtle) {
+                ((Turtle) s).swim();
+            }
+            if (s.getClass().getName().equals("com.company.Turtle")) {
+                ((Turtle) s).swim();
+            }
+            if (s instanceof Eagle) {
+                ((Eagle) s).fly();
+            }
+            if (s.getClass().getName().equals("com.company.Eagle")) {
+                ((Eagle) s).fly();
+            }
+            System.out.println();
+        }
+        Animal[] reptile = {turtle};
+        Animal[] fish = {shark};
+        Animal[] bird = {eagle};
+    }
+}
